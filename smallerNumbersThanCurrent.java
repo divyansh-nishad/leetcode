73838d4f4ec7748b18c5c04c1e39ddce6cc943eb
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-04-13 20:07:14 +0530

    smallerNumbersThanCurrent

---
+++ smallerNumbersThanCurrent.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) 
+    {
+        int l=nums.length;
+        int ret[] =new int[l];int i,j;
+        for(i=0;i<l;i++)
+        {
+            for(j=0;j<l;j++)
+            {
+                if(i!=j)
+                {
+                    if(nums[i]>nums[j])
+                        ret[i]=ret[i]+1;
+                }
+            }
+        }
+        return ret;
+    }
+}
