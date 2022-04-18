f98788791942fdfff4a01bcae6ac8b6df2835107
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-04-18 22:43:29 +0530

    Kth Smallest Element

---
+++ kth_smallest_element.java
@@ -0,0 +1,39 @@
+public class kth_smallest_element {
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k - 1 - count);
+        }
+        return root.val;
+    }
+
+    public int countNodes(TreeNode n) {
+        if (n == null)
+            return 0;
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+
+    // ***************************
+    // Only for treenode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // ***************************
+}
