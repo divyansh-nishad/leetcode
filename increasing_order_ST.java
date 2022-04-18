caad2472faf0cf4b0b85ad8030d7504b4b050599
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-04-18 22:47:21 +0530

    Increasing Order ST

---
+++ increasing_order_ST.java
@@ -0,0 +1,41 @@
+public class increasing_order_ST {
+    TreeNode prev = null, head = null;
+
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null)
+            return null;
+        increasingBST(root.left);
+        if (prev != null) {
+            root.left = null;
+            prev.right = root;
+        }
+        if (head == null)
+            head = root;
+        prev = root;
+        increasingBST(root.right);
+        return head;
+
+    }
+
+    // ***************************
+    // Only for treenode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // ***************************
+}
