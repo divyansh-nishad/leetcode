0ed9ae24529be27a69ea0bb2991bb19c0a31a3ba
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-04-18 22:44:57 +0530

    Search BST

--- searchBST.java
+++ searchBST.java
@@ -1,12 +1,33 @@
 public class searchBST {
-    public TreeNode searchBST(TreeNode root, int val) 
-    {
-        if(root==null || root.val==val)
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val)
             return root;
-        if(val<root.val)
+        if (val < root.val)
             return searchBST(root.left, val);
         else
             return searchBST(root.right, val);
-        
-    }  
+
+    }
+
+    // ***************************
+    // Only for treenode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // ***************************
 }
