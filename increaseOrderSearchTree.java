8b19f500ffdb63b37efa21287a257e54eba32147
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-04-29 22:05:09 +0530

    Increasing order Search Tree

---
+++ increaseOrderSearchTree.java
@@ -0,0 +1,35 @@
+public class increaseOrderSearchTree {
+    TreeNode prev = null, head = null;
+
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null)
+            return null;
+        increasingBST(root.left);
+        if (prev != null) {
+            root.left = null;
+            prev.right = root;
+        }
+        if (head == null)
+            head = root;
+        prev = root;
+        increasingBST(root.right);
+        return head;
+
+    }
+
+    // **************************************
+    // for TreeNode
+    public class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode() {}
+             TreeNode(int val) { this.val = val; }
+             TreeNode(int val, TreeNode left, TreeNode right) {
+                 this.val = val;
+                 this.left = left;
+                 this.right = right;
+             }
+        s }
+
+}
