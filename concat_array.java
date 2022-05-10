9ecf802d0acef13811718af6ff11631fe2a88c2a
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-05-10 16:49:30 +0530

    Concatenation of Array

---
+++ concat_array.java
@@ -0,0 +1,13 @@
+public class concat_array {
+    public int[] getConcatenation(int[] nums) {
+        int l = nums.length;
+        int l2 = 2 * l;
+        int arr[] = new int[l2];
+        for (int i = 0; i < l; i++)
+            arr[i] = nums[i];
+        for (int i = 0; i < l; i++)
+            arr[i + l] = nums[i];
+        return arr;
+
+    }
+}
