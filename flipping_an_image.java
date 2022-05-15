fef1d39d5ddd4e20084643c9e1762a350892d8bc
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-05-15 22:47:16 +0530

    Flipping An Image

---
+++ flipping_an_image.java
@@ -0,0 +1,11 @@
+public class flipping_an_image {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int[] row : A)
+            for (int i = 0; i * 2 < n; i++)
+                if (row[i] == row[n - i - 1])
+                    row[i] = row[n - i - 1] ^= 1;
+        return A;
+    }
+    
+}
