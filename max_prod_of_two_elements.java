88b7b8fa7d999f5a716a196947426c4030b37fb4
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-05-31 22:43:50 +0530

    Maximum Product of two elements in a array

---
+++ max_prod_of_two_elements.java
@@ -0,0 +1,10 @@
+public class max_prod_of_two_elements 
+{
+    public int maxProduct(int[] nums) 
+    {
+        int l=nums.length;
+        Arrays.sort(nums);
+        return((nums[l-1]-1)*(nums[l-2]-1));
+    }
+    
+}
