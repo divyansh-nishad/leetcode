b42bead5cfa0a74e7ce3dcb8bf5384343d30f038
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-06-07 19:10:33 +0530

    Decode XORED Array

--- decodeXORedarray.java
+++ decodeXORedarray.java
@@ -1,10 +1,11 @@
-public class decodeXORedarray 
-{
-    int n = A.length, res[] = new int[n + 1];
-    res[0] = first;
-    for (int i = 0; i < n; ++i)
-        res[i + 1] = res[i] ^ A[i];
-    return res;
+public class decodeXORedarray {
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length, res[] = new int[n + 1];
+        res[0] = first;
+        for (int i = 0; i < n; ++i)
+            res[i + 1] = res[i] ^ encoded[i];
+        return res;
 
-    
+    }
+
 }
