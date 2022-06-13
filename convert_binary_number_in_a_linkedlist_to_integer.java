19b0e661e258d8d58d0f6deb9f7f32c08fa648f7
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-06-13 18:08:39 +0530

    Optimize

--- convert_binary_number_in_a_linkedlist_to_integer.java
+++ convert_binary_number_in_a_linkedlist_to_integer.java
@@ -1,7 +1,6 @@
 public class convert_binary_number_in_a_linkedlist_to_integer {
 
     public int getDecimalValue(ListNode head) {
-        String t = "";
         ListNode temp = head;
         int value = 0;
         while (temp != null) {
