6e9e4d3f491805a1a2d3de87bd4815e8b2768f63
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-06-20 22:15:43 +0530

    XOR operation in an array

---
+++ xor_operation_in_an_array.java
@@ -0,0 +1,10 @@
+public class xor_operation_in_an_array {
+    public int xorOperation(int n, int start) {
+        int res = start;
+        for (int i = 1; i < n; i++) {
+            res = res ^ (start + 2 * i);
+        }
+        return res;
+    }
+
+}
