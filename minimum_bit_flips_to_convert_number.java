3872e5c0067dc4d006cc63b6e29f27a23644fcf8
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-06-21 22:16:22 +0530

    update

--- minimum_bit_flips_to_convert_number.java
+++ minimum_bit_flips_to_convert_number.java
@@ -1,9 +1,13 @@
 public class minimum_bit_flips_to_convert_number {
-    int xor = start ^ goal;
-    int count=0;while(xor>0)
-    {
-        count++;
-        xor = xor & (xor - 1);
-    }return count;
+    public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+        int count = 0;
+        while (xor > 0) {
+            count++;
+            xor = xor & (xor - 1);
+        }
+        return count;
+
+    }
 
 }
