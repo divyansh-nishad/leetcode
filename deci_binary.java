ec98fa33d017826ebc53efcba9a71411d7cfd8c3
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-06-27 18:35:03 +0530

    Deci Binary

---
+++ deci_binary.java
@@ -0,0 +1,9 @@
+public class deci_binary {
+    public int minPartitions(String n) {
+        int res = 0;
+        for (int i = 0; i < n.length(); ++i)
+            res = Math.max(res, n.charAt(i) - '0');
+        return res;
+    }
+
+}
