569e29bd7941fdf802ba2b2d979ea5d866827486
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-07-21 10:40:24 +0530

    Number Complement

---
+++ number_complement.java
@@ -0,0 +1,10 @@
+public class number_complement {
+    public int findComplement(int num) {
+        int n = 0;
+        while (n < num) {
+            n = (n << 1) | 1;
+        }
+        return n - num;
+    }
+
+}
