80be4040a2164bc15c6b0c72ee704bc29b371c30
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-08 11:20:52 +0530

    3SumClosest

---
+++ sum3_closest.java
@@ -0,0 +1,23 @@
+import java.util.Collections;
+
+public class sum3_closest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int x = i + 1;
+            int y = nums.length - 1;
+            while (x < y) {
+                int sum = nums[i] + nums[x] + nums[y];
+                if (Math.abs(target - sum) < Math.abs(diff))
+                    diff = (target - sum);
+                if (sum < target)
+                    x++;
+                else
+                    y--;
+            }
+        }
+        return target - diff;
+    }
+
+}
