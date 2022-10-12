2c449b3656cf77bccba3483a322fa8944c0187cd
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-12 17:27:02 +0530

    Symmetric Tree

--- symmetricTree.java
+++ symmetricTree.java
@@ -13,5 +13,23 @@ public class symmetricTree {
             return true;
         return helper(root.left,root.right);
     }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     
 }
