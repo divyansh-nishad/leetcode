30a091d4a06d463fb8c641788f9bd14f2bcaa6f0
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-13 17:03:44 +0530

    Delete Node In Linked List Added TreeNode

--- delete_node_in_linked_list.java
+++ delete_node_in_linked_list.java
@@ -3,4 +3,22 @@ public class delete_node_in_linked_list {
         node.val = node.next.val;
         node.next = node.next.next;
     }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
