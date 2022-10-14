327ecaf988417c5a06b8f7ea72952d5d258c4147
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-14 16:55:17 +0530

    Delete the Middle Node of The Linked List

---
+++ delete_the_middle_node_of_a_libked_list.java
@@ -0,0 +1,14 @@
+public class delete_the_middle_node_of_a_libked_list {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+        ListNode p = head, q = head.next.next;
+        while (q != null && q.next != null) {
+            p = p.next;
+            q = q.next.next;
+        }
+        p.next = p.next.next;
+        return head;
+    }
+
+}
