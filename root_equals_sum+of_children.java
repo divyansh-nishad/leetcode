6f017c712d5f74e4d64ec7153bd6e195f1f7b222
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-15 20:15:58 +0530

    Updated TreeNode

--- root_equals_sum+of_children.java
+++ root_equals_sum+of_children.java
@@ -3,5 +3,22 @@ public class root_equals_sum+of_children {
         return ((root.left.val+root.right.val)==root.val);
         
     }
-    
+}
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
