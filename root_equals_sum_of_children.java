30fe8a733853e303bb8eca5df5de2613ce4573e3
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-16 14:56:17 +0530

    Resolved Errors

---
+++ root_equals_sum_of_children.java
@@ -0,0 +1,26 @@
+public class root_equals_sum_of_children {
+
+    public boolean checkTree(TreeNode root) {
+        return ((root.left.val + root.right.val) == root.val);
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
