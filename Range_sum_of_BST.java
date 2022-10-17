edf1365f5e574e033a8f6b730d3f0f254f5c9f5d
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-17 16:53:13 +0530

    Updated TreeNode In Range Sum Of BST

--- Range_sum_of_BST.java
+++ Range_sum_of_BST.java
@@ -10,4 +10,23 @@ public class Range_sum_of_BST {
         }
 
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
