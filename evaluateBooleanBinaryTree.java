85fbb5fe4232f75bad0115f434fa4b808e2d1f17
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-18 21:13:43 +0530

    Evaluate Boolean Binary Tree

--- evaluateBooleanBinaryTree.java
+++ evaluateBooleanBinaryTree.java
@@ -1,20 +1,36 @@
 public class evaluateBooleanBinaryTree {
-    public boolean evaluateTree(TreeNode root) 
-    {
-        if(root==null)
+    public boolean evaluateTree(TreeNode root) {
+        if (root == null)
             return true;
-        if(root.val==0)
+        if (root.val == 0)
             return false;
-        else if(root.val==1)
+        else if (root.val == 1)
             return true;
-        else
-        {
-            if(root.val==2)
+        else {
+            if (root.val == 2)
                 return evaluateTree(root.left) || evaluateTree(root.right);
-            else if(root.val==3)
+            else if (root.val == 3)
                 return evaluateTree(root.left) && evaluateTree(root.right);
         }
         return false;
     }
-    
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
