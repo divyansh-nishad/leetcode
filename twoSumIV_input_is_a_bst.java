a3bf83601733917ead7badf0d04b871cc9decb50
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-19 23:33:35 +0530

    Two Sum IV

--- twoSumIV_input_is_a_bst.java
+++ twoSumIV_input_is_a_bst.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class twoSumIV_input_is_a_bst {
     private boolean helper(TreeNode root, HashSet<Integer> hs, int k) {
         if (root == null)
@@ -15,4 +17,22 @@ public class twoSumIV_input_is_a_bst {
 
     }
 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
