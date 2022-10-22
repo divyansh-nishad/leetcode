96a7f774317aaa1237b663e12df8687ae25261a7
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-22 18:47:32 +0530

    All Possible Full Binary Trees

---
+++ AllPossibleFullBinaryTrees.java
@@ -0,0 +1,26 @@
+public class AllPossibleFullBinaryTrees {
+    public List<TreeNode> allPossibleFBT(int n) {
+        if ((n <= 0) || n % 2 == 0)
+            return new ArrayList<TreeNode>();
+        List<TreeNode> ans = new ArrayList<TreeNode>();
+        if (n == 1) {
+            ans.add(new TreeNode(0));
+            return ans;
+        }
+        for (int i = 1; i < n; i += 2) {
+            List<TreeNode> l = allPossibleFBT(i);
+            List<TreeNode> r = allPossibleFBT(n - i - 1);
+            for (TreeNode lf : l) {
+                for (TreeNode rr : r) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = lf;
+                    root.right = rr;
+                    ans.add(root);
+                }
+            }
+        }
+        return ans;
+
+    }
+
+}
