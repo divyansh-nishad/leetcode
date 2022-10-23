ce1497ba54765c6e068afd3a9cda5374c6c5907d
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-23 14:11:16 +0530

    Letter Combinations Of a Phone Number

---
+++ LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,24 @@
+public class LetterCombinationsOfAPhoneNumber {
+    public List<String> letterCombinations(String digits) {
+        return helper("", digits);
+    }
+
+    private List<String> helper(String p, String up) {
+
+        if (up.length() == 0) {
+            ArrayList<String> list = new ArrayList<String>();
+            if (p.length() != 0)
+                list.add(p);
+            return list;
+        } else {
+            ArrayList<String> list = new ArrayList<String>();
+            String[] alp = { "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            String str = alp[Integer.parseInt("" + up.charAt(0)) - 1];
+            for (int i = 0; i < str.length(); i++) {
+                list.addAll(helper(p + str.charAt(i), up.substring(1)));
+            }
+            return list;
+        }
+
+    }
+}
