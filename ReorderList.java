d6f6380693ab41631040bdb0ee21d0174ba01b1d
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-24 18:24:30 +0530

    ListNode

--- ReorderList.java
+++ ReorderList.java
@@ -17,4 +17,21 @@ public class ReorderList {
         reorderList(temp);
         
     }    
+    public class ListNode {
+        int val;
+        ListNode next;
+    
+        ListNode() {
+        }
+    
+        ListNode(int val) {
+            this.val = val;
+        }
+    
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    
+    }
 }
