190651aad799abfcdaa3f7c41887c08228aa4588
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-29 18:50:43 +0530

    Subsets

---
+++ Subsets.java
@@ -0,0 +1,17 @@
+public class Subsets{
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> up=new ArrayList<>();
+        generate(nums,0,new ArrayList<>(),up);
+        return up;
+    }
+    public void generate(int[] nums,int i,List<Integer> p,List<List<Integer>> up){
+        if(i==nums.length){
+            up.add(new ArrayList(p));
+            return;
+        }
+        generate(nums,i+1,p,up);
+        p.add(nums[i]);
+        generate(nums,i+1,p,up);
+        p.remove(p.size()-1);
+    }
+}
