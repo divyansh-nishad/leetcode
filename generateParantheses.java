88a2d2ed3f7488ab5af2ddefbf3d3c5ca3e9043b
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-10-31 19:17:25 +0530

    Generate Parantheses

---
+++ generateParantheses.java
@@ -0,0 +1,21 @@
+public class generateParantheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<String>();
+        helper(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    private void helper(List<String> list, String str, int open, int close, int n) {
+        if (str.length() == 2 * n) {
+            list.add(str);
+            return;
+        }
+        if (open < n) {
+            helper(list, str + "(", open + 1, close, n);
+        }
+        if (close < open) {
+            helper(list, str + ")", open, close + 1, n);
+        }
+    }
+
+}
