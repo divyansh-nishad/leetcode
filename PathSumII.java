489dcb8db71b69cd40c1a0c35909c4c90c179c02
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-11-02 22:13:37 +0530

    Path Sum II

---
+++ PathSumII.java
@@ -0,0 +1,23 @@
+public class PathSumII {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        List<Integer> list = new ArrayList<>();
+        helper(ans, list, root, targetSum);
+        return ans;
+    }
+
+    private void helper(List<List<Integer>> ans, List<Integer> list, TreeNode root, int target) {
+        list.add(root.val);
+        if (root.left == null && root.right == null)
+            if (target == root.val)
+                ans.add(new ArrayList<>(list));
+        if (root.left != null)
+            helper(ans, list, root.left, target - root.val);
+        if (root.right != null)
+            helper(ans, list, root.right, target - root.val);
+        list.remove(list.size() - 1);
+    }
+
+}
