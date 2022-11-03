54c321cd98eda8000d0d277da125ce52ff06e371
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-11-03 20:03:31 +0530

    SubsetsII

---
+++ SubsetsII.java
@@ -0,0 +1,23 @@
+public class SubsetsII {
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) 
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        helper(ans, list, 0, nums, nums.length);
+        return ans;
+    }
+    private void helper(List<List<Integer>> ans, List<Integer> list, int ind, int[] nums, int n)
+    {
+        ans.add(new ArrayList<>(list));
+        for(int i=ind;i<n;i++)
+        {
+                if(i>ind && nums[i]==nums[i-1])
+                    continue;
+                list.add(nums[i]);
+                helper(ans, list, i+1, nums, n);
+                list.remove(list.size()-1);
+        }
+    }
+}
