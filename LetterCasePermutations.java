43d3ef0c2734317f71965db6f1d5190202ee775d
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-11-04 18:36:07 +0530

    Letter Case Permutation

---
+++ LetterCasePermutations.java
@@ -0,0 +1,25 @@
+public class LetterCasePermutations {
+    public List<String> letterCasePermutation(String s) {
+        int n=s.length();
+        s=s.toLowerCase();
+        List<String> ans=new ArrayList<>();
+        helper(ans, 0, "", n, s);
+        return ans;        
+    }
+    private void helper(List<String> ans, int ind, String res, int n, String s)
+    {
+        if(ind==n)
+        {
+            ans.add(res);
+            return;
+        }
+        else
+        {
+            char ch=s.charAt(ind);
+            if(Character.isLetter(ch))
+                helper(ans, ind+1, res + Character.toUpperCase(ch), n, s);
+            helper(ans, ind+1, res + (ch), n, s);
+        }
+    }
+    
+}
