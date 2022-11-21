a3980f07529c9260528d4e1d4058a457cbe707b1
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-11-21 12:48:31 +0530

    Deepest Leaves Sum

---
+++ DeepestLeavesSum.java
@@ -0,0 +1,25 @@
+public class DeepestLeavesSum {
+    int sum = 0;
+
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null)
+            return sum;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int level = queue.size();
+            sum = 0;
+            for (int i = 0; i < level; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                sum += queue.poll().val;
+            }
+        }
+        return sum;
+    }
+
+}
