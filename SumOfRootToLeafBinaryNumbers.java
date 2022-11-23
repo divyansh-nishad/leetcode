e358865b95896e8bf156a8e073a88095ef66bc8a
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-11-23 15:12:28 +0530

    SumOfRootToLeafBinaryNumbers

--- SumOfRootToLeafBinaryNumbers.java
+++ SumOfRootToLeafBinaryNumbers.java
@@ -18,4 +18,25 @@ public class SumOfRootToLeafBinaryNumbers {
         }
     }
 
+    // ****************************************************************
+    // For treeNode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // ****************************************************************
 }
