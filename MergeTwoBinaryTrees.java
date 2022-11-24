04bf5b82ede114178df49dcc1b1f7e18bbd455d3
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-11-24 22:02:25 +0530

    Merge Two Binary Trees

--- MergeTwoBinaryTrees.java
+++ MergeTwoBinaryTrees.java
@@ -1,24 +1,40 @@
 public class MergeTwoBinaryTrees {
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) 
-    {
-        if(root1==null && root2==null)
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null)
             return null;
-        else if(root1==null)
-        {
+        else if (root1 == null) {
             return root2;
-        }
-        else if(root2==null)
-        {
+        } else if (root2 == null) {
             return root1;
-        }
-        else
-        {
-            TreeNode temp=new TreeNode();
-            temp.val=root1.val+root2.val;
-            temp.left=mergeTrees(root1.left,root2.left);
-            temp.right=mergeTrees(root1.right,root2.right);
+        } else {
+            TreeNode temp = new TreeNode();
+            temp.val = root1.val + root2.val;
+            temp.left = mergeTrees(root1.left, root2.left);
+            temp.right = mergeTrees(root1.right, root2.right);
             return temp;
         }
     }
-    
+
+    // ****************************************************************
+    // For treeNode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // ****************************************************************
+
 }
