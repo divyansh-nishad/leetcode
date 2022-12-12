76cd52d1325e4479ae248d4552ddd7f9d1089b51
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-12 13:20:28 +0530

    Is Ugly

---
+++ IsUgly.java
@@ -0,0 +1,17 @@
+public class IsUgly {
+    public boolean isUgly(int n) {
+        if (n == 0)
+            return false;
+        while (n % 5 == 0)
+            n = n / 5;
+        while (n % 3 == 0)
+            n = n / 3;
+        while (n % 2 == 0)
+            n = n / 2;
+        if (n == 1)
+            return true;
+        return false;
+
+    }
+
+}
