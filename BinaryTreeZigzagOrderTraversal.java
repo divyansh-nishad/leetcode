90fd9ce883407f60db6f283bfd32f206fa8e0b06
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-16 19:24:27 +0530

    Binary Tree Zigzag order traversal

---
+++ BinaryTreeZigzagOrderTraversal.java
@@ -0,0 +1,30 @@
+public class BinaryTreeZigzagOrderTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> ans = new LinkedList<>();
+        if(root==null)
+        return ans;
+        queue.offer(root);
+        boolean ltf=true;
+        while(!queue.isEmpty())
+        {
+            int level = queue.size();
+            List<Integer> res = new LinkedList<Integer>();
+            for(int i=0; i<level;i++)
+            {
+                if(queue.peek().left!=null)
+                    queue.offer(queue.peek().left);
+                if(queue.peek().right!=null)
+                    queue.offer(queue.peek().right);
+                if(ltf)
+                res.add(queue.poll().val);
+                else
+                res.add(0 ,queue.poll().val);
+            }
+            ans.add(res);
+            ltf=!ltf;
+        }
+        return ans;
+    }
+    
+}
