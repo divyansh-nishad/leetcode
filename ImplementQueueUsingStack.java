3f14d400a8d867963f5b707c91c39fe82b420432
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-16 14:38:48 +0530

    Implement Queue using Stacks

---
+++ ImplementQueueUsingStack.java
@@ -0,0 +1,25 @@
+public class ImplementQueueUsingStack {
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+    
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+    
+}
