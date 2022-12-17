de20cff04e8d16f617693c1c403d55f02c09ef37
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-17 10:14:09 +0530

    Evaluate Reverse Polish Notation

---
+++ EvaluateReversePolishNotation.java
@@ -0,0 +1,30 @@
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) 
+    {
+        int n1, n2;
+        Stack<Integer> st = new Stack<Integer>();
+        for(int i=0;i<tokens.length;i++)
+        {
+            if(Character.isDigit(tokens[i].charAt(0))||(tokens[i].length()>1))
+            {
+                st.push(Integer.parseInt(tokens[i]));
+            }
+            else
+            {
+                n2=st.pop();
+                n1=st.pop();
+                if(tokens[i].equals("*"))
+                    st.push(n1*n2);
+                else if(tokens[i].equals("/"))
+                    st.push(n1/n2);
+                else if(tokens[i].equals("+"))
+                    st.push(n1+n2);
+                else if(tokens[i].equals("-"))
+                    st.push(n1-n2);
+            }
+        } 
+        int val=st.pop();
+        System.out.println(val);
+        return val;  
+    }
+}
