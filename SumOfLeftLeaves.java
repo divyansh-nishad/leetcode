b733f264a8cad1e734f12642f16364140f18e92d
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-21 19:35:35 +0530

    -sum of left leaves

--- SumOfLeftLeaves.java
+++ SumOfLeftLeaves.java
@@ -13,4 +13,25 @@ public class SumOfLeftLeaves {
 
     }
 
+    // ****************************************************************
+    // For treeNode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // ****************************************************************
 }
