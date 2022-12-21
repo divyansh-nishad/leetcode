294f36a9f7d4bf5151a9d65757409f6938a6b463
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-21 19:36:02 +0530

    -binary tree tilt

--- BinaryTreeTilt.java
+++ BinaryTreeTilt.java
@@ -23,4 +23,26 @@ public class BinaryTreeTilt {
 
     }
 
+    // ****************************************************************
+    // For treeNode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // ****************************************************************
+
 }
