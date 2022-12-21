f76f269f6a125979de4995e2c59bcc9775f76c68
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-21 19:36:45 +0530

    construct string from binary tree

--- construct_string_from_binary_tree.java
+++ construct_string_from_binary_tree.java
@@ -14,3 +14,25 @@ public class construct_string_from_binary_tree {
     }
 
 }
+
+// ****************************************************************
+// For treeNode
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+// ****************************************************************
