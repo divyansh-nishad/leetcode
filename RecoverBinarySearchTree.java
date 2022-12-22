d314d28c8579dc393f54ede20ad04e671ee86ade
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-22 19:42:48 +0530

    Recover Binary Search Tree

---
+++ RecoverBinarySearchTree.java
@@ -0,0 +1,35 @@
+public class RecoverBinarySearchTree {
+    private TreeNode first, middle, last, prev;
+
+    public void recoverTree(TreeNode root) {
+        first = middle = last = null;
+        prev = new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
+        if (first != null && last != null) {
+            int t = first.val;
+            first.val = last.val;
+            last.val = t;
+        } else if (first != null && middle != null) {
+            int t = first.val;
+            first.val = middle.val;
+            middle.val = t;
+        }
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null)
+            return;
+        inorder(root.left);
+        if (prev != null && (root.val < prev.val)) {
+            if (first == null) {
+                first = prev;
+                middle = root;
+            } else {
+                last = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
+    }
+
+}
