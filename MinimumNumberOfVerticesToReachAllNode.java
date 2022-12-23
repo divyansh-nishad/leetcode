df201c54e471d3bec637482badb5ec3f39d91a56
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-23 20:46:59 +0530

    Minimum Number of Nodes To Reach All Nodes

---
+++ MinimumNumberOfVerticesToReachAllNode.java
@@ -0,0 +1,13 @@
+public class MinimumNumberOfVerticesToReachAllNode {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        List<Integer> ans = new ArrayList<>();
+        boolean vis[] = new boolean[n + 1];
+        for (List<Integer> lis : edges)
+            vis[lis.get(1)] = true;
+        for (int i = 0; i < n; i++)
+            if (vis[i] == false)
+                ans.add(i);
+        return ans;
+    }
+
+}
