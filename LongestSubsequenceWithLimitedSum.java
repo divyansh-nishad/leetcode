22983fa571556108d9f50de4d3ba72463beb258c
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-25 15:01:35 +0530

    Longest Subsequence with limited sum

---
+++ LongestSubsequenceWithLimitedSum.java
@@ -0,0 +1,14 @@
+public class LongestSubsequenceWithLimitedSum {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int n = nums.length, m = queries.length, res[] = new int[m];
+        for (int i = 1; i < n; ++i)
+            nums[i] += nums[i - 1];
+        for (int i = 0; i < m; ++i) {
+            int j = Arrays.binarySearch(nums, queries[i]);
+            res[i] = Math.abs(j + 1);
+        }
+        return res;
+    }
+
+}
