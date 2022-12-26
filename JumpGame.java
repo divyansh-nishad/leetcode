8e55319bdf8b61909e326421617654d75def31e5
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2022-12-26 11:09:53 +0530

    Jump Game

---
+++ JumpGame.java
@@ -0,0 +1,15 @@
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i>max) 
+            {
+                return false;
+            }
+            max = Math.max(nums[i]+i,max);
+        }
+    return true;
+    }
+    
+}
