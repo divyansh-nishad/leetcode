e2c5cfba99cf5a7923275080d12e3dfc81eabbea
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-01-16 22:02:49 +0530

    -insert interval

---
+++ InsertInterval.java
@@ -0,0 +1,36 @@
+public class InsertInterval {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        List<int[]> result = new ArrayList<>();
+
+        // Iterate through all slots
+        for (int[] slot : intervals) {
+
+            // if newInterval before slot insert newInterval & update slot as new interval
+            if (newInterval[1] < slot[0]) {
+                result.add(newInterval);
+                newInterval = slot;
+            }
+
+            // if slot is lesser than new Interval insert slot
+            else if (slot[1] < newInterval[0]) {
+                result.add(slot);
+            }
+
+            // if above conditions fail its an overlap since possibility of new interval
+            // existing in left & right of slot is checked
+            // update lowest of start & highest of end & not insert
+            else {
+                newInterval[0] = Math.min(newInterval[0], slot[0]);
+                newInterval[1] = Math.max(newInterval[1], slot[1]);
+            }
+        }
+
+        // insert the last newInterval
+        result.add(newInterval);
+
+        // convert to int[][] array
+        return result.toArray(new int[result.size()][]);
+    }
+
+}
