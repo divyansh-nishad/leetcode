61caaec30501377d29a96f8c4a36d9dabde453eb
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-02-20 21:53:04 +0530

    -assign cookies

--- AssignCookies.java
+++ AssignCookies.java
@@ -1,23 +1,22 @@
+import java.util.Arrays;
+
 public class AssignCookies {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int gi=0;
-        int si=0;
-        int count =0;
-        while(gi<g.length && si<s.length)
-        {
-            if(s[si]>=g[gi])
-            {
+        int gi = 0;
+        int si = 0;
+        int count = 0;
+        while (gi < g.length && si < s.length) {
+            if (s[si] >= g[gi]) {
                 count++;
                 si++;
                 gi++;
-            }
-            else
+            } else
                 si++;
 
         }
         return count;
     }
-    
+
 }
