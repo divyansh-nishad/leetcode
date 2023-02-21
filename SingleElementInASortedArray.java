4d723a2dd369ae2bacd2643dbb2f7aff50632eba
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-02-21 10:47:04 +0530

    -single element in a sorted array

---
+++ SingleElementInASortedArray.java
@@ -0,0 +1,27 @@
+public class SingleElementInASortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        int low = 0;
+        int high = nums.length - 1;
+
+        if (nums[0] != nums[1])
+            return nums[0];
+
+        if (nums[high] != nums[high - 1])
+            return nums[high];
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                return nums[mid];
+            else if ((nums[mid] == nums[mid + 1] && mid % 2 == 0) || (nums[mid] == nums[mid - 1] && mid % 2 != 0))
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return nums[low];
+    }
+
+}
