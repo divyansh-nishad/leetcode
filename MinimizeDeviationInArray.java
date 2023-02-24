edd42e21f7999465eb2a6a62d3396563101e71e4
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-02-24 15:29:42 +0530

    -minimize deviation in array

---
+++ MinimizeDeviationInArray.java
@@ -0,0 +1,26 @@
+public class MinimizeDeviationInArray {
+    public int minimumDeviation(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(nums.length, Collections.reverseOrder());
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                pq.offer(num);
+                min = Math.min(min, num);
+            } else {
+                pq.offer(num * 2);
+                min = Math.min(min, num * 2);
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        while (pq.peek() % 2 == 0) {
+            int n = pq.poll();
+            ans = Math.min(ans, n - min);
+            int m = n / 2;
+            pq.offer(m);
+            min = Math.min(min, m);
+        }
+        ans = Math.min(pq.peek() - min, ans);
+        return ans;
+    }
+
+}
