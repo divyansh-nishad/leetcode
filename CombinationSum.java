00d8e9c3e19a22ae4de884bf2a0455bd64638cab
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-02-25 22:34:00 +0530

    -combination sum

--- CombinationSum.java
+++ CombinationSum.java
@@ -1,25 +1,25 @@
+import java.util.*;
+
 public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> list =new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         helper(ans, list, candidates, target, 0);
         return ans.stream().distinct().collect(Collectors.toList());
     }
-    private void helper(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start)
-    {
-        if(remain < 0) 
+
+    private void helper(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain < 0)
             return;
-        else if(remain == 0) 
+        else if (remain == 0)
             list.add(new ArrayList<>(tempList));
-        else
-        { 
-            for(int i = start; i < nums.length; i++)
-            {
+        else {
+            for (int i = start; i < nums.length; i++) {
                 tempList.add(nums[i]);
-                helper(list, tempList, nums, remain - nums[i], i); 
+                helper(list, tempList, nums, remain - nums[i], i);
                 tempList.remove(tempList.size() - 1);
             }
         }
     }
-    
+
 }
