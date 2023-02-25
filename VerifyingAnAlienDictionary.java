ec03d88871bb92c63a6a7658dfa3b1586b59ba84
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-02-25 22:31:24 +0530

    -verify an alien dictionary

--- VerifyingAnAlienDictionary.java
+++ VerifyingAnAlienDictionary.java
@@ -1,15 +1,21 @@
+import java.util.*;
+
 public class VerifyingAnAlienDictionary {
 
+    Map<Character, Integer> map;
+
+    public boolean isAlienSorted(String[] words, String order) {
         map = new HashMap<>();
         for (int i = 0; i < order.length(); i++) {
             map.put(order.charAt(i), i);
         }
         for (int i = 0; i < words.length - 1; i++) {
-            if (!compare(words[i], words[i + 1])) return false;
+            if (!compare(words[i], words[i + 1]))
+                return false;
         }
         return true;
     }
-    
+
     private boolean compare(String s1, String s2) {
         int l1 = s1.length(), l2 = s2.length();
         for (int i = 0, j = 0; i < l1 && j < l2; i++, j++) {
@@ -21,8 +27,8 @@ public class VerifyingAnAlienDictionary {
                 }
             }
         }
-        if (l1 > l2) return false;
+        if (l1 > l2)
+            return false;
         return true;
     }
-    
 }
