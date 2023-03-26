c9388589a3502adc3c06733b960c6ce9649959d5
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-03-26 21:56:35 +0530

    -longest cycle in a graph

--- LongestCycleInAGraph.java
+++ LongestCycleInAGraph.java
@@ -1,23 +1,23 @@
 public class LongestCycleInAGraph {
-    int res = -1;
-    boolean[] vis = new boolean[edges.length]; // global visisted
+    public int longestCycle(int[] edges) {
+        int res = -1;
+        boolean[] vis = new boolean[edges.length]; // global visisted
 
-    for(
-    int i = 0;i<edges.length;i++)
-    {
-        if (vis[i])
-            continue;
-        HashMap<Integer, Integer> x = new HashMap<>(); // local visited
-        for (int idx = i, dist = 0; idx != -1; idx = edges[idx]) {
-            if (x.containsKey(idx)) {
-                res = Math.max(res, dist - x.get(idx));
-                break;
+        for (int i = 0; i < edges.length; i++) {
+            if (vis[i])
+                continue;
+            HashMap<Integer, Integer> x = new HashMap<>(); // local visited
+            for (int idx = i, dist = 0; idx != -1; idx = edges[idx]) {
+                if (x.containsKey(idx)) {
+                    res = Math.max(res, dist - x.get(idx));
+                    break;
+                }
+                if (vis[idx])
+                    break;
+                vis[idx] = true;
+                x.put(idx, dist++);
             }
-            if (vis[idx])
-                break;
-            vis[idx] = true;
-            x.put(idx, dist++);
         }
-    }return res;
-
+        return res;
+    }
 }
