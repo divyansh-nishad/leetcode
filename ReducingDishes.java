4f372334097549834824e85ae024441c90eab705
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-03-29 21:40:47 +0530

    -reducing dishes

---
+++ ReducingDishes.java
@@ -0,0 +1,15 @@
+public class ReducingDishes {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int totalSatisfaction = 0;
+        int maxTotalSatisfaction = 0;
+        int numDishes = satisfaction.length;
+
+        for (int i = numDishes - 1; i >= 0 && satisfaction[i] > -totalSatisfaction; --i) {
+            totalSatisfaction += satisfaction[i];
+            maxTotalSatisfaction += totalSatisfaction;
+        }
+        return maxTotalSatisfaction;
+    }
+
+}
