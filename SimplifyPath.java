fa84ec76e036eed82984bd160c4d25c15b3cc31e
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-04-12 09:13:19 +0530

    -simplify path

---
+++ SimplifyPath.java
@@ -0,0 +1,17 @@
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..", ".", ""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty())
+                stack.pop();
+            else if (!skip.contains(dir))
+                stack.push(dir);
+        }
+        String res = "";
+        for (String dir : stack)
+            res = res + "/" + dir;
+        return res.isEmpty() ? "/" : res;
+    }
+
+}
