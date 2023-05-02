634128db207aa9942e8c7f4d142a1137b88cb4e4
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-05-02 09:44:35 +0530

    - sign of the product of ana array

---
+++ SignOfTheProductOfAnArray.java
@@ -0,0 +1,16 @@
+public class SignOfTheProductOfAnArray {
+    public int arraySign(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                return 0;
+            else if (nums[i] < 0)
+                count += 1;
+        }
+        if (count % 2 == 0)
+            return 1;
+        else
+            return -1;
+    }
+
+}
