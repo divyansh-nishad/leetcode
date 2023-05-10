fdbe2b8bc4d17cb66d6129b8bbe7e15e8e1b7577
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-05-10 21:56:07 +0530

    -find closest node to given two nodes

--- spiralMatrix.java
+++ spiralMatrix.java
@@ -1,38 +1,32 @@
 public class spiralMatrix {
-    public int[][] generateMatrix(int n) 
-    {
-        int copy=n;
-        int ar[][]=new int[n][n];
-        int i=0,j=0;
-        ar[i][j]=1;
-        int c=1;
-        int base=0;
-        while(c!=(copy*copy))
-        {
-            while(j!=n-1)
-            {
+    public int[][] generateMatrix(int n) {
+        int copy = n;
+        int ar[][] = new int[n][n];
+        int i = 0, j = 0;
+        ar[i][j] = 1;
+        int c = 1;
+        int base = 0;
+        while (c != (copy * copy)) {
+            while (j != n - 1) {
                 j++;
-                ar[i][j]=++c;
+                ar[i][j] = ++c;
             }
-            while(i!=n-1)
-            {
+            while (i != n - 1) {
                 i++;
-                ar[i][j]=++c;
+                ar[i][j] = ++c;
             }
-            while(j!=base)
-            {
+            while (j != base) {
                 j--;
-                ar[i][j]=++c;
+                ar[i][j] = ++c;
             }
-            while(i>base+1)
-            {
+            while (i > base + 1) {
                 i--;
-                ar[i][j]=++c;
+                ar[i][j] = ++c;
             }
             base++;
             n--;
         }
         return ar;
     }
-    
+
 }
