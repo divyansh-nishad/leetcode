1bf5f79bc731d1ccbcbaf3adbcc2d39eba28f297
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-05-15 21:58:09 +0530

    - swapping node in a linked list

---
+++ SwappingNodesInALinkedlList.java
@@ -0,0 +1,22 @@
+public class SwappingNodesInALinkedlList {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode first = head, second = head;
+        for (int i = 0; i < k - 1; ++i)
+            fast = fast.next;
+        first = fast;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        second = slow;
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+
+        return head;
+    }
+
+}
