d8d58b9e1b48fbd0dbfbd3c2a6f0bb99a9e45a3d
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-05-22 19:39:08 +0530

    -top k frequent elements

---
+++ TopKFrequentElements.java
@@ -0,0 +1,20 @@
+public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        for (int key : map.keySet()) {
+            heap.add(key);
+        }
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = heap.poll();
+        }
+
+        return ans;
+    }
+}
