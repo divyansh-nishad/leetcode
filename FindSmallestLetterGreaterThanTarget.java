1f0e810f22c1a937b34be736e09e318fbee81e53
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-06-09 22:17:00 +0530

    - find smallest letter greater than target

---
+++ FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,21 @@
+public class FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char res = letters[0];
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] == target) {
+                start = mid + 1;
+            } else if (target > letters[mid]) {
+                start = mid + 1;
+            } else if (letters[mid] > target) {
+                res = letters[mid];
+                end = mid - 1;
+            }
+        }
+        return res;
+
+    }
+
+}
