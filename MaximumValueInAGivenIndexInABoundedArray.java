c46bb4e626995492c1e88142bb4fdd145da6f686
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-06-10 20:00:23 +0530

    maximum value in bounded array

---
+++ MaximumValueInAGivenIndexInABoundedArray.java
@@ -0,0 +1,23 @@
+public class MaximumValueInAGivenIndexInABoundedArray {
+    public int maxValue(int n, int index, int maxSum) {
+        maxSum -= n;
+        int left = 0, right = maxSum, mid;
+        while (left < right) {
+            mid = (left + right + 1) / 2;
+            if (test(n, index, mid) <= maxSum)
+                left = mid;
+            else
+                right = mid - 1;
+        }
+        return left + 1;
+    }
+
+    private long test(int n, int index, int a) {
+        int b = Math.max(a - index, 0);
+        long res = (long) (a + b) * (a - b + 1) / 2;
+        b = Math.max(a - ((n - 1) - index), 0);
+        res += (long) (a + b) * (a - b + 1) / 2;
+        return res - a;
+    }
+
+}
