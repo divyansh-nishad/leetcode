2e44b5f8132ba6ee16ff6e96d80cd736d9cddcde
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-06-14 21:44:19 +0530

    contains duplicate ii

--- ContainsDuplicateII.java
+++ ContainsDuplicateII.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         Set<Integer> set = new HashSet<Integer>();
