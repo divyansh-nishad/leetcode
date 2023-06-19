239fb24b08a839b1cdb1f44e58c4937d1fc069db
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-06-19 19:54:46 +0530

    reverse integer

---
+++ ReverseInteger.java
@@ -0,0 +1,13 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+        long r = 0;
+        while (x != 0) {
+            r = r * 10 + x % 10;
+            x = x / 10;
+        }
+        if (r > Integer.MAX_VALUE || r < Integer.MIN_VALUE)
+            return 0;
+        return (int) r;
+    }
+
+}
