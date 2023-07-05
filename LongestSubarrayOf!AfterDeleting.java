83454e0b227937f4dd7a7cee42681ea19ba22add
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-07-05 19:02:29 +0530

    - longest subarray of 1 after deleting one element

---
+++ LongestSubarrayOf!AfterDeleting.java
@@ -0,0 +1,24 @@
+public class LongestSubarrayOf!AfterDeleting {
+    public int longestSubarray(int[] nums) {
+        int n = nums.length;
+
+        int left = 0;
+        int zeros = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+            while (zeros > 1) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1 - zeros);
+        }
+        return (ans == n) ? ans - 1 : ans;
+    }
+    
+}
