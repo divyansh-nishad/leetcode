d556aa19918c3299963b14c34c4abf2b5f62b6d2
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-07-23 20:05:53 +0530

    -pascal triangle

--- PascalTriangleII.java
+++ PascalTriangleII.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalTriangleII {
     public List<Integer> getRow(int rowIndex) {
         List<List<Integer>> ans = new ArrayList<>();
