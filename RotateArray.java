23694687870558b8dbe585d00ffa89a64bb96b5c
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-02 11:47:54 +0530

    -optimal solution added

--- RotateArray.java
+++ RotateArray.java
@@ -15,3 +15,28 @@ public class RotateArray {
         }
     }
 }
+class Solution {
+    //most optimal solution
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n; // In case k is larger than the length of the array, take the modulo.
+
+        // Reverse the first n-k elements.
+        reverseArray(nums, 0, n - k - 1);
+
+        // Reverse the rest elements.
+        reverseArray(nums, n - k, n - 1);
+
+        // Reverse the entire array.
+        reverseArray(nums, 0, n - 1);
+    }
+    private static void reverseArray(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
