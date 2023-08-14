b8609793e50d9119336ed3a273695c92226a09e4
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-14 23:11:29 +0530

    - kth largest element

---
+++ KthLargestElementInArray.java
@@ -0,0 +1,16 @@
+public class KthLargestElementInArray {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            pq.add(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (pq.peek() < nums[i]) {
+                pq.poll();
+                pq.add(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
+
+}
