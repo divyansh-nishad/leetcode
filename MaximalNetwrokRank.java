2082392e35004811db251ed11faa63401b3dc5d6
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-18 23:10:20 +0530

    -maximal network rank

---
+++ MaximalNetwrokRank.java
@@ -0,0 +1,57 @@
+public class MaximalNetwrokRank {
+    class Solution {
+        public int maximalNetworkRank(int n, int[][] roads) {
+            int degrees[] = new int[n];
+            for(int arr[]:roads)
+            {
+                int a =arr[0];
+                int b = arr[1];
+                degrees[a]+=1;
+                degrees[b]+=1;
+            }
+            Set<Integer> uniqueDegrees = new HashSet<>();
+            for(int degree:degrees)
+            {
+                uniqueDegrees.add(degree);
+            }
+    
+            List<Integer> sortedDegrees = new ArrayList<>(uniqueDegrees);
+            Collections.sort(sortedDegrees, Collections.reverseOrder());
+    
+            int maxDeg = sortedDegrees.get(0);
+            int secondMaxDeg = sortedDegrees.size()>1?sortedDegrees.get(1):0
+    ;
+            int maxCount =0;
+            for(int degree:degrees)
+            {
+                if(degree==maxDeg)
+                    maxCount++;
+            }
+            int secondMaxCount = 0;
+            for(int degree:degrees)
+            {
+                if(degree==secondMaxDeg)
+                    secondMaxCount++;
+            }
+            if (maxCount > 1) {
+                int directlyConnected = 0;
+                for (int[] road : roads) {
+                    if (degrees[road[0]] == maxDeg && degrees[road[1]] == maxDeg)
+                        directlyConnected++;
+                }
+                int possibleConnections = maxCount * (maxCount - 1) / 2;
+                return possibleConnections == directlyConnected ? 2 * maxDeg - 1 : 2 * maxDeg;
+            }
+    
+            int directConnectionsToSecond = 0;
+            for (int[] road : roads) {
+                if ((degrees[road[0]] == maxDeg && degrees[road[1]] == secondMaxDeg) ||
+                    (degrees[road[0]] == secondMaxDeg && degrees[road[1]] == maxDeg))
+                    directConnectionsToSecond++;
+            }
+    
+            return secondMaxCount == directConnectionsToSecond ? maxDeg + secondMaxDeg - 1 : maxDeg + secondMaxDeg;
+        }
+    }   
+    
+}
