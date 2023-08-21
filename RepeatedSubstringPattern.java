923acc7fc999dfb0ab14e66505ef5933378f519c
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-21 18:29:39 +0530

    -repeated substring pattern

--- RepeatedSubstringPattern.java
+++ RepeatedSubstringPattern.java
@@ -1,19 +1,19 @@
 public class RepeatedSubstringPattern {
-    public boolean repeatedSubstringPattern(String s) {
+    public boolean repeatedSubstringPattern(String s) 
+    {
         int l = s.length();
-        for (int i = l / 2; i >= 1; i--) {
-            if (l % i == 0) {
-                int m = l / i;
-                String subS = s.substring(0, i);
-                String ans = "";
-                for (int j = 0; j < m; j++) {
-                    ans += (subS);
-                }
-                if (ans.equals(s))
-                    return true;
-            }
-        }
-        return false;
+	    for(int i=l/2;i>=1;i--) {
+		    if(l%i==0) {
+			    int m = l/i;
+			    String subS = s.substring(0,i);
+					StringBuilder sb = new StringBuilder();
+			    for(int j=0;j<m;j++) {
+						sb.append(subS);
+			    }
+			    if(sb.toString().equals(s)) return true;
+		    }
+	    }
+	    return false;
     }
 
 }
