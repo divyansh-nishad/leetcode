e46bbe70c9eefe2b818702a7185d2c9adaf75397
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-22 19:40:23 +0530

    -excel sheet title

--- excelsheet_column_title.java
+++ excelsheet_column_title.java
@@ -1,15 +1,12 @@
 public class excelsheet_column_title {
-    public String convertToTitle(int columnNumber) {
-        StringBuilder sb = new StringBuilder();
-        String result = "";
-        while (columnNumber > 0) {
-            columnNumber--;
-            result = result + (char) ('A' + columnNumber % 26);
-            columnNumber /= 26;
+    public String convertToTitle(int columnNumber) 
+    {
+        StringBuilder result = new StringBuilder();
+        while(columnNumber!=0){
+            result.append((char)((int) 'A' + (columnNumber-1)% 26));
+            columnNumber = (columnNumber - 1)/26;
         }
-        sb.append(result);
-        sb.reverse();
-        return sb.toString();
-
+        return result.reverse().toString();
+        
     }
 }
