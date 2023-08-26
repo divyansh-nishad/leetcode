db4eae051d470fbc99f0e67fad810112402337a1
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-08-26 23:59:16 +0530

    -pascal's triangle

--- PascalTriangleII.java
+++ PascalTriangleII.java
@@ -2,23 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangleII {
-    public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=1;i<=rowIndex+1;i++)
+    public List<Integer> getRow(int row) {
+        List<Integer> list = new ArrayList<>();
+        long start=1;
+        list.add((int)start);
+        for(int i=0;i<row;i++)
         {
-            List<Integer> list = new ArrayList<>();
-            for(int j=0;j<i;j++)
-            {
-                if(j==0 || j==i-1)
-                    list.add(1);
-                else
-                    list.add(ans.get(i-2).get(j-1)+ans.get(i-2).get(j));
-            }
-            ans.add(list);
-            if(i==rowIndex+1)
-                return list;
+            start*=(row-i);
+            start/=(i+1);
+            list.add((int)start);
         }
-        return new ArrayList<>();
+        return list;
     }
     
 }
