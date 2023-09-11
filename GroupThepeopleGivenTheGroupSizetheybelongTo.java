3ef71476502f1dad14ec390741fd6f89b5b60095
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-09-11 20:39:37 +0530

    -group th epeople

---
+++ GroupThepeopleGivenTheGroupSizetheybelongTo.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupThepeopleGivenTheGroupSizetheybelongTo {
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        HashMap<Integer, List<Integer>> temp_group = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < groupSizes.length; i++) {
+            int size = groupSizes[i];
+            temp_group.putIfAbsent(size, new ArrayList<>());
+            temp_group.get(size).add(i);
+
+            if (temp_group.get(size).size() == size) {
+                result.add(new ArrayList<>(temp_group.get(size)));
+                temp_group.get(size).clear();
+            }
+        }
+        return result;
+    }
+
+}
