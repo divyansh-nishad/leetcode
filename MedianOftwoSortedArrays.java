8ace12a20445a5d9069e62f450a4b6ff7fefea6e
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-09-21 23:43:59 +0530

    -median of two sorted arrays

--- MedianOftwoSortedArrays.java
+++ MedianOftwoSortedArrays.java
@@ -1,50 +1,31 @@
 public class MedianOftwoSortedArrays {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1==null && nums2==null)
-        {
-            return 0;
-        }
-        if(nums1==null)
-        {
-            int n=nums2.length;
-            return nums2[(n-1)/2]*0.5+nums2[n/2]*0.5;
-        }
-        if(nums2==null)
-        {
-            int n=nums1.length;
-            return nums1[(n-1)/2]*0.5+nums1[n/2]*0.5;
-        }
-        if(nums1.length>nums2.length)
-        {
-            return findMedianSortedArrays(nums2,nums1);
-        }
-        int n = nums1.length;
-        int m= nums2.length;
-        int left=0;
-        int right=n;
-        while(left<right)
-        {
-            int i= left+(right-left)/2;
-            int j=(n+m)/2-i;
-            if(nums1[i]<nums2[j-1])
-                left=i+1;
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        int n1 = a.length, n2 = b.length;
+        if (n1 > n2)
+            return findMedianSortedArrays(b, a);
+
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2;
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? a[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? b[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1)
+                    return Math.max(l1, l2);
+                else
+                    return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > r2)
+                high = mid1 - 1;
             else
-                right = i;
+                low = mid1 + 1;
         }
-        int first = left;
-        int second = (n+m)/2-left;
-
-        int shortLeft = first==0?Integer.MIN_VALUE:nums1[first-1];
-        int shortRight = first==n?Integer.MAX_VALUE:nums1[first];
-
-        int longLeft = second==0?Integer.MIN_VALUE:nums2[second-1];
-        int longRight = second==m?Integer.MAX_VALUE:nums2[second];
-
-        if((n+m)%2==1)
-            return Math.min(shortRight,longRight);
-        else
-            return Math.max(shortLeft, longLeft) * 0.5 + Math.min(shortRight, longRight) * 0.5;
+        return 0;
     }
 
-    
 }
