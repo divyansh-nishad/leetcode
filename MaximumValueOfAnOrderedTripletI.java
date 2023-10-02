8aa0b0890bf83de33f1fdb44a94e9b6dedb00d49
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-10-02 17:41:27 +0530

    - maximum value of ordered triplet i

---
+++ MaximumValueOfAnOrderedTripletI.java
@@ -0,0 +1,21 @@
+public class MaximumValueOfAnOrderedTripletI {
+    public long maximumTripletValue(int[] nums) {
+        long pre[] = new long[nums.length];
+        long suf[] = new long[nums.length];
+        long ans = 0;
+        pre[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pre[i] = Math.max(pre[i - 1], nums[i]);
+        }
+        suf[nums.length - 1] = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suf[i] = Math.max(suf[i + 1], nums[i]);
+        }
+        for (int i = 1; i < nums.length - 1; i++) {
+            // System.out.println(pre[i-1]+" "+suf[i+1]);
+            ans = Math.max(ans, (pre[i - 1] - nums[i]) * suf[i + 1]);
+        }
+        return ans;
+    }
+
+}
