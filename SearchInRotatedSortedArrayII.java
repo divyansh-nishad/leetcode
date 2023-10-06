8d2b9c713ac8c699c7b3b258caa51c6515bdb308
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-10-07 00:14:38 +0530

    -search in rotated sorted array ii

--- SearchInRotatedSortedArrayII.java
+++ SearchInRotatedSortedArrayII.java
@@ -1,33 +1,28 @@
 public class SearchInRotatedSortedArrayII {
-    class Solution {
-        public boolean search(int[] nums, int target) {
-            int start = 0, end = nums.length - 1, mid = -1;
-            while (start <= end) {
-                mid = (start + end) / 2;
-                if (nums[mid] == target) {
-                    return true;
-                }
-                if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
-                    if (target > nums[mid] && target <= nums[end]) {
-                        start = mid + 1;
-                    } else {
-                        end = mid - 1;
-                    }
-
-                } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
-                    if (target < nums[mid] && target >= nums[start]) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
-
+    public boolean search(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target)
+                return true;
+            else if (nums[lo] == nums[mid] && nums[mid] == nums[hi]) {
+                lo++;
+                hi--;
+            } else if (nums[lo] <= nums[mid]) {
+                if (nums[lo] <= target && target <= nums[mid]) {
+                    hi = mid - 1;
                 } else {
-                    end--;
+                    lo = mid + 1;
+                }
+            } else {
+                if (target <= nums[hi] && target >= nums[mid]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
                 }
             }
-
-            return false;
         }
+        return false;
     }
-
 }
