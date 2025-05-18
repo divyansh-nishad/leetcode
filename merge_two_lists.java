5b203d5cf958b277c72c57edf89d26dee7cb4e7c
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2025-05-18 18:04:18 +0530

    -optimized merge two list

--- merge_two_lists.java
+++ merge_two_lists.java
@@ -1,16 +1,21 @@
 public class merge_two_lists {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        if (list1 == null)
-            return list2;
-        if (list2 == null)
-            return list1;
-        if (list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null)
+                return list2;
+            else if(list2 == null)
+                return list1;
+            
+            if (list1.val<=list2.val)
+            {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
         }
     }
 
